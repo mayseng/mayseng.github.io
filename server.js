19aab5a3dcf1e47729f1a73f147063decf7537ac
mayseng <graberamaysen@gmail.com>
2025-03-20 21:27:36 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const jwt =
