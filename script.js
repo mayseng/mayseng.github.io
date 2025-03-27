d241d8626f80c4f05431a920b353d2acea61bb85
mayseng <graberamaysen@gmail.com>
2025-03-27 04:16:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+const character = document.getElementById('character');
+let posX = 0;
+let posY = 0;
 
-document.getElementById('colorButton').addEventListener('click', function() {
-    const newColor = getRandomColor();
-    document.getElementById('colorDisplay').style.backgroundColor = newColor;
-    document.getElementById('colorDisplay').innerText = newColor;
+document.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'w': // Move up
+            posY -= 10;
+            break;
+        case 's': // Move down
+            posY += 10;
+            break;
+        case 'a': // Move left
+            posX -= 10;
+            break;
+        case 'd': // Move right
+            posX += 10;
+            break;
+    }
+    character.style.transform = `translate(${posX}px, ${posY}px)`;
 });
