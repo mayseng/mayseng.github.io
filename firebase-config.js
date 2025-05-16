011f2300091374c715c69284a445d8a7562cbc9e
mayseng <graberamaysen@gmail.com>
2025-05-16 10:15:46 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Import necessary Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyC3NzI9YQuX1auwT0Wsmis9qAC1M_LdhHI",
   authDomain: "secretministrationcom.firebaseapp.com",
@@ -11,7 +9,6 @@ const firebaseConfig = {
   projectId: "secretministrationcom",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
